df366c8b7b727c45e3b40259a60b371e8407abad
raco45 <raco190@gmail.com>
2022-03-30 21:26:10 -0400

    final commit

--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -5,16 +5,7 @@
  */
 package proyecto2;
 
-import DesicionTree.ArbolD;
-import HashTable.HashTable;
 import Interfaz.Menu;
-import Lectura.ArbolHash;
-import Lectura.ArchivoTxt;
-import MostrarArbol.Controlador;
-import MostrarArbol.Mostrar;
-import Partidas.Partida;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 
 /**
  *
@@ -30,27 +21,7 @@ public class Proyecto2 {
             Menu ventana = new Menu();
             ventana.setVisible(true);
         
-//        JFileChooser archivo= new JFileChooser();
-//        archivo.showOpenDialog(archivo);
-//        String path=archivo.getSelectedFile().getAbsolutePath();
-//        System.out.println(path);
-//        ArchivoTxt prueba= new ArchivoTxt();
-//        ArbolHash nuevo=prueba.leerTxt("test\\archivo.csv");
-//        ArbolD arbolito= nuevo.getArbolito();
-//        HashTable tabla=nuevo.getTable();
-//        
-//        Partida par= new Partida();
-//        par.juego(arbolito);
-//
-//
-//        Mostrar obj = new Mostrar();
-//        Controlador control = new Controlador(obj, arbolito);
-//        control.iniciar();
-//        JFrame vent = new JFrame();
-//        vent.getContentPane().add(obj);
-//        vent.setDefaultCloseOperation(3);
-//        vent.setSize(600, 600);
-//        vent.setVisible(true);
+
         
     }
     
